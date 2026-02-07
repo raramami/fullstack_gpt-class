7cd399b41d1d7145a4d6cd3a2f01db7e4c1bb545
raramami <icbest@naver.com>
2026-02-07 12:32:42 +0900

    revised

--- app.py
+++ app.py
@@ -114,6 +114,7 @@ def main():
     llm = ChatOpenAI( 
         temperature=0.1,
         streaming=True,
+        openai_api_key=openai_api_key,
         callbacks=[
             ChatCallBackHandler()
             ],
